e803b3f5b820be094030ef89a5de4412dd1d7c27
cairumeng <cairumeng@gmail.com>
2021-02-20 14:44:53 +0100

    layout header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,25 @@
 import Login from './containers/Login/Login'
+import Layout from './components/Layout/Layout'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <Layout>
+        <Router>
+          <Switch>
+            <Route path="/login">
+              <Login />
+            </Route>
+          </Switch>
+        </Router>
+      </Layout>
+      <Login />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
+
+const Header = (props) => {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="/">Livrensemble</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/login">Login</Nav.Link>
+          <Nav.Link href="/register">Register</Nav.Link>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-success">Search</Button>
+        </Form>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+}
+
+export default Header
