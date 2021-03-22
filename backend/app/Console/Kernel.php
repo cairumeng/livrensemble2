34aa2f477dd8fc8dea177857df28cfa2028f67fa
cairumeng <cairumeng@gmail.com>
2021-03-22 10:06:33 +0100

    command clear

--- backend/app/Console/Kernel.php
+++ backend/app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\ClearUnvalidCommands;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,7 +14,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        ClearUnvalidCommands::class
     ];
 
     /**
@@ -24,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('commands:clear')->everyThirtyMinutes();
     }
 
     /**
@@ -34,7 +35,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
--- backend/app/Http/Controllers/CommandsController.php
+++ backend/app/Http/Controllers/CommandsController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\City;
 use App\Models\Command;
+use Illuminate\Http\Request;
 
 class CommandsController extends Controller
 {
@@ -10,5 +12,13 @@ class CommandsController extends Controller
     public function show($id)
     {
         return Command::with('restaurant.categories.dishes')->find($id);
+    }
+
+    public function index(Request $request)
+    {
+        $cityId = $request->query('cityId', 1);
+        $city = City::find($cityId);
+        $cityCommands = Command::where('city_id', $cityId)->where('is_valid', 1)->with('restaurant')->get();
+        return response()->json(['city' => $city, 'cityCommands' => $cityCommands]);
     }
 }
